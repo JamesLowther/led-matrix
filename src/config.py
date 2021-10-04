ab41a58689c1cbd663e053640ae3d0f1aa8fcd25
James Lowther <jamesevlowther@gmail.com>
2021-10-04 12:24:13 -0600

    Update

--- src/config.py
+++ src/config.py
@@ -1,7 +1,9 @@
 import os
 from dotenv import dotenv_values
 
+# Paths.
 SRC_BASE = os.path.dirname(__file__)
 FONTS = os.path.join(SRC_BASE, "assets", "fonts")
 
+# Environment variables.
 ENV_VALUES = dotenv_values()
